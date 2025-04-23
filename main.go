4c02209f7309c73a14e3623586cd6c1695edb411
JXSnack <jx.snack@gmail.com>
2025-04-23 17:51:04 +0200

    config

--- main.go
+++ main.go
@@ -1,8 +1,22 @@
 package main
 
-import "github.com/snackbag/compass/compass"
+import (
+	"github.com/snackbag/compass/compass"
+	"github.com/snackbag/wiki/global"
+)
 
 func main() {
 	server := compass.NewServer()
+	handler := global.NewHandler()
+
+	config := AssembleConfiguration(handler)
+
+	server.Port = config.Port
+	server.StaticDirectory = config.StaticDir
+	server.TemplatesDirectory = config.TemplatesDir
+	server.ComponentsDirectory = config.ComponentsDir
+
+	global.SetHandlerServerResponse(handler, &server)
+
 	server.Start()
 }
