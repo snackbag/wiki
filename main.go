b11074f72f350952ea8cd3404447ffc090db4507
JXSnack <jx.snack@gmail.com>
2025-04-23 17:57:26 +0200

    dont start at all if config is nil

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/snackbag/compass/compass"
 	"github.com/snackbag/wiki/global"
 )
@@ -10,6 +11,10 @@ func main() {
 	handler := global.NewHandler()
 
 	config := AssembleConfiguration(handler)
+	if config == nil {
+		fmt.Println("Config is nil, aborting startup.")
+		return
+	}
 
 	server.Port = config.Port
 	server.StaticDirectory = config.StaticDir
