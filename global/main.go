657125ce8fe3fca805b101a5d10965ca1bb08d5e
JXSnack <jx.snack@gmail.com>
2025-04-23 17:51:01 +0200

    this

--- global/main.go
+++ global/main.go
@@ -1,6 +1,10 @@
 package global
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/snackbag/compass/compass"
+	"strings"
+)
 
 type Handler struct {
 	Errors []string
@@ -14,4 +18,26 @@ func (handler *Handler) DoFatalError(err string) {
 
 func NewHandler() *Handler {
 	return &Handler{Errors: make([]string, 0)}
+}
+
+func SetHandlerServerResponse(handler *Handler, server *compass.Server) {
+	server.SetBeforeRequestHandler(func(request compass.Request) *compass.Response {
+		if len(handler.Errors) > 0 {
+			ctx := compass.NewTemplateContext(server)
+
+			builder := strings.Builder{}
+			for _, value := range handler.Errors {
+				builder.WriteString("<li>")
+				builder.WriteString(value)
+				builder.WriteString("</li>\n")
+			}
+
+			ctx.SetVariable("errors", builder.String())
+
+			resp := compass.Fill("fatal_error.html", ctx, server)
+			return &resp
+		}
+
+		return nil
+	})
 }
